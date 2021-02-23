cc2915c6eea9e06c88c13de02b2a8a2836af9ee4
Kshitij Girkar <girkarkshitij@gmail.com>
2021-02-23 12:37:01 +0530

    Fix: Checks in users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
 
@@ -10,16 +10,15 @@ const User = require('../models/User');
 router.post(
   '/',
   [
-    // TODO: Checks are not working
-    check('name').not().isEmpty(),
-    check('email').isEmail(),
+    check('name', 'Please enter your name').not().isEmpty(),
+    check('email', 'Please enter a valid email address').isEmail(),
     check('password', 'Password should contain minimum 8 characters').isLength({
       min: 8,
     }),
   ],
   (req, res) => {
     const err = validationResult(req);
-    if (!err.isEmpty) {
+    if (!err.isEmpty()) {
       return res.status(400).json({ errors: err.array() });
     }
     res.send('Passed ✔️');
