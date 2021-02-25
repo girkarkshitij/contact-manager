c57817aa0053a4e2f35045878525fb8ed0998840
Kshitij Girkar <girkarkshitij@gmail.com>
2021-02-25 15:55:55 +0530

    Add login function in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const User = require('../models/User');
+const secret = process.env.JWT_SECRET;
 
 // @route    GET api/auth
 // @desc     Get logged in user
@@ -11,8 +18,48 @@ router.get('/', (req, res) => {
 // @route    POST api/auth
 // @desc     Auth user & get token
 // @access   Public
-router.post('/', (req, res) => {
-  res.send('Log in ✔️');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Please enter a valid email address').isEmail(),
+    check('password', 'Please enter your password').exists(),
+  ],
+  async (req, res) => {
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+      return res.status(400).json({ errors: err.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: 'This email is not registered' });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        return res.status(400).json({ msg: 'Wrong password' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, secret, { expiresIn: 360000 }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
