365879660765adc2b9c513b306df46ea63c267ca
Kshitij Girkar <girkarkshitij@gmail.com>
2021-02-26 15:06:49 +0530

    Complete GET request in auth.js to get logged in user

--- routes/auth.js
+++ routes/auth.js
@@ -6,13 +6,20 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const User = require('../models/User');
+const auth = require('../middleware/auth');
 const secret = process.env.JWT_SECRET;
 
 // @route    GET api/auth
 // @desc     Get logged in user
 // @access   Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user 👨');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route    POST api/auth
