9bf60989a206cdf0d1323952915807c68b68cd37
Kshitij Girkar <girkarkshitij@gmail.com>
2021-03-03 15:14:39 +0530

    Add home and about pages

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='card all-center'>
+      <h1>About</h1>
+      <p>This is a MERN app to manage contacts</p>
+      <p>
+        Made with <i className='fas fa-heart'></i> by Kshitij
+      </p>
+    </div>
+  );
+};
+
+export default About;
