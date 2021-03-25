1d0748402f413bde4838f2e3e4ba67a2f17012fa
girkarkshitij <girkarkshitij@gmail.com>
2021-03-25 19:35:12 +0530

    Add ContactItem.js to display individual contact

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,65 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
+
+const ContactItem = ({ contact }) => {
+  const { id, name, email, phone, type } = contact;
+  const contactContext = useContext(ContactContext);
+  const {
+    deleteContact,
+    setCurrentContact,
+    clearCurrentContact,
+  } = contactContext;
+
+  const onDelete = () => {
+    deleteContact(id);
+    clearCurrentContact();
+  };
+
+  return (
+    <div className='card bg-light'>
+      <h3 className='text-primary text-left'>
+        {name}{' '}
+        <span
+          className={
+            'badge ' + (type === 'Personal' ? 'badge-primary' : 'badge-success')
+          }
+          style={{ float: 'right' }}
+        >
+          {type}
+        </span>
+      </h3>
+      <ul className='list'>
+        {email && (
+          <li>
+            <i className='fas fa-envelope'></i>
+            {' ' + email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-phone-square-alt'></i>
+            {' ' + phone}
+          </li>
+        )}
+      </ul>
+      <div>
+        <button
+          className='btn btn-dark btn-sm'
+          onClick={() => setCurrentContact(contact)}
+        >
+          <i className='fas fa-edit'></i>
+        </button>
+        <button className='btn btn-sm btn-danger' onClick={onDelete}>
+          <i className='fas fa-trash-alt'></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired,
+};
+
+export default ContactItem;
