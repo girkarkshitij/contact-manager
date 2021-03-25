1ce426d70374ef2dd119692b79e74a8681c2a45d
girkarkshitij <girkarkshitij@gmail.com>
2021-03-25 19:39:41 +0530

    Add Contacts.js

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,31 @@
+import React, { Fragment, useContext } from 'react';
+import ContactContext from '../../context/contact/contactContext';
+import ContactItem from './ContactItem';
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return (
+      <h3 className='card text-center'>
+        <i className='far fa-frown'></i> No contacts to show
+      </h3>
+    );
+  }
+
+  return (
+    <Fragment>
+      {filtered !== null
+        ? filtered.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
+    </Fragment>
+  );
+};
+
+export default Contacts;
