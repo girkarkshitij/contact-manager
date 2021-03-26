e1c83320d496de9e95ef5695bb1efe0636de2f90
girkarkshitij <girkarkshitij@gmail.com>
2021-03-26 20:02:17 +0530

    Create sign up form

---
+++ client/src/components/auth/SignUp.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+const SignUp = () => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = user;
+
+  const onChange = (e) =>
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('Sign Up Done');
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>
+        Sign <span className='text-primary'>Up</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name' value={name} onChange={onChange} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Email</label>
+          <input type='email' name='email' value={email} onChange={onChange} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password2'>Confirm Password</label>
+          <input
+            type='password'
+            name='password2'
+            value={password2}
+            onChange={onChange}
+          />
+        </div>
+        <input
+          type='submit'
+          value='Signup'
+          className='btn btn-block btn-primary'
+        />
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
