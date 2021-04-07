e16dc60bb2cee10c4e0f071f42512afa4606d74b
girkarkshitij <girkarkshitij@gmail.com>
2021-04-07 12:54:32 +0530

    Update navbar links according to login status

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
 
 const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, logout, user } = authContext;
+
+  const onLogout = () => {
+    logout();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>
+        <i className='fas fa-user'></i> {user && user.name}
+      </li>
+      <li>
+        <a onClick={onLogout} href='#!'>
+          <span className='hide-sm'>
+            <i className='fas fa-sign-out-alt'></i>Log Out{' '}
+          </span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to='/signup'>Sign Up</Link>
+      </li>
+      <li>
+        <Link to='/login'>Log In</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className='navbar bg-primary'>
       <h1>
         <i className={icon} />
         {' ' + title}
       </h1>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Sign Up</Link>
-        </li>
-        <li>
-          <Link to='/login'>Log In</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-      </ul>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
     </div>
   );
 };
